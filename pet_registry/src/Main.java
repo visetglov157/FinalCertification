72722529bf8b4175acd0617acbb25307091d4659
Vladislav <115417107+visetglov157@users.noreply.github.com>
2024-02-14 10:37:58 +0300

    changes

--- pet_registry/src/Main.java
+++ pet_registry/src/Main.java
@@ -1,1 +1,15 @@
+package src;
+import controller.Controller;
+import model.FileOperation;
+import model.Repository;
+import view.View;
 
+public class Main {
+    public static void main(String[] args) {
+        FileOperation fileOperation = new FileOperation("animals.csv");
+        Repository repository = new Repository(fileOperation);
+        Controller controller = new Controller(repository);
+        View view = new View(controller);
+        view.run();
+    }
+}
